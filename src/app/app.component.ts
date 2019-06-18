f5b95a3add53e10a19e4b6139a39b702a918ff0a
antkormai <antkorsc@gmail.com>
2019-06-18 21:15:22 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   counter = 0;
-  decreaseDisabledFlag = false
+  decreaseDisabledFlag = false;
 
   increase() {
     this.counter++;
